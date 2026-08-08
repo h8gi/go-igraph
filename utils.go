a4c7556b97dd7da13746faa98084fc53cd1e819b
hiroki yagi <hiro.yagi0245@gmail.com>
2026-08-08 22:59:25 +0900

    support igraph boolean values

--- utils.go
+++ utils.go
@@ -1,10 +1,13 @@
+pyenv: cannot rehash: /Users/yagi/.pyenv/shims isn't writable
 package igraph
 
+// #include <igraph.h>
+// static igraph_bool_t go_igraph_bool(int value) { return value != 0; }
 import "C"
 
-func booltoint(in bool) C.int {
+func booltoint(in bool) C.igraph_bool_t {
 	if in {
-		return C.int(1)
+		return C.go_igraph_bool(1)
 	}
-	return C.int(0)
+	return C.go_igraph_bool(0)
 }
