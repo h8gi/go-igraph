6b7b75d7dd3ba02341a9d5067e7c23ca5de56be0
hiroki yagi <hiro.yagi0245@gmail.com>
2026-08-22 09:53:42 +0900

    Add pair and edge similarity APIs

--- similarity_cgo.c
+++ similarity_cgo.c
@@ -35,3 +35,33 @@ igraph_error_t go_igraph_bibcoupling(
         igraph_vs_t vertices) {
     GO_IGRAPH_CALL(igraph_bibcoupling(graph, result, vertices));
 }
+
+igraph_error_t go_igraph_similarity_jaccard_pairs(
+        const igraph_t *graph, igraph_vector_t *result,
+        const igraph_vector_int_t *pairs,
+        igraph_neimode_t mode, igraph_bool_t loops) {
+    GO_IGRAPH_CALL(igraph_similarity_jaccard_pairs(
+        graph, result, pairs, mode, loops));
+}
+
+igraph_error_t go_igraph_similarity_dice_pairs(
+        const igraph_t *graph, igraph_vector_t *result,
+        const igraph_vector_int_t *pairs,
+        igraph_neimode_t mode, igraph_bool_t loops) {
+    GO_IGRAPH_CALL(igraph_similarity_dice_pairs(
+        graph, result, pairs, mode, loops));
+}
+
+igraph_error_t go_igraph_similarity_jaccard_es(
+        const igraph_t *graph, igraph_vector_t *result,
+        igraph_es_t edges, igraph_neimode_t mode, igraph_bool_t loops) {
+    GO_IGRAPH_CALL(igraph_similarity_jaccard_es(
+        graph, result, edges, mode, loops));
+}
+
+igraph_error_t go_igraph_similarity_dice_es(
+        const igraph_t *graph, igraph_vector_t *result,
+        igraph_es_t edges, igraph_neimode_t mode, igraph_bool_t loops) {
+    GO_IGRAPH_CALL(igraph_similarity_dice_es(
+        graph, result, edges, mode, loops));
+}
