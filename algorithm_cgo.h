83f447c6e733bf707c6d0dd2c93cf8db21ae3725
hiroki yagi <hiro.yagi0245@gmail.com>
2026-08-31 19:58:47 +0900

    feat: add subset betweenness APIs

--- algorithm_cgo.h
+++ algorithm_cgo.h
@@ -145,6 +145,12 @@ igraph_error_t go_igraph_betweenness(
 igraph_error_t go_igraph_edge_betweenness(
     const igraph_t *, const igraph_vector_t *, igraph_vector_t *,
     igraph_es_t, igraph_bool_t, igraph_bool_t, igraph_bool_t, igraph_real_t);
+igraph_error_t go_igraph_betweenness_subset(
+    const igraph_t *, const igraph_vector_t *, igraph_vector_t *,
+    igraph_vs_t, igraph_vs_t, igraph_vs_t, igraph_bool_t);
+igraph_error_t go_igraph_edge_betweenness_subset(
+    const igraph_t *, const igraph_vector_t *, igraph_vector_t *,
+    igraph_vs_t, igraph_vs_t, igraph_es_t, igraph_bool_t);
 igraph_error_t go_igraph_eigenvector_centrality(
     const igraph_t *, igraph_vector_t *, igraph_real_t *, igraph_neimode_t,
     const igraph_vector_t *, int, igraph_real_t);
